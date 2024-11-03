dee1730fad1448c20a814374e2260bdcd10c3cd1
nathandxn <nathan.dixon@snyk.io>
2024-11-03 18:14:39 -0500

    feat: terraform add multi-instances

--- main.tf
+++ main.tf
@@ -19,8 +19,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-005fc0f236362e99f" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.medium"
+resource "aws_security_group" "instances" {
+  name = "instance-security-group"
 }
 
+resource "aws_instance" "instance_1" {
+  ami           = "ami-005fc0f236362e99f" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instances.name]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World 1" > index.html
+              python3 -m http.server 8080 &
+              EOF
+}
+
+resource "aws_instance" "instance_2" {
+  ami             = "ami-005fc0f236362e99f" # Ubuntu 20.04 LTS // us-east-1
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instances.name]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World 2" > index.html
+              python3 -m http.server 8080 &
+              EOF
+}
