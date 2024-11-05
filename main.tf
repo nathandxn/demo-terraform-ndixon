b8212c2defde3d008c86bfed08200485c775615b
nathandxn <nathan.dixon@snyk.io>
2024-11-05 14:42:34 -0500

    feat: add s3 bucket sse

--- main.tf
+++ main.tf
@@ -58,3 +58,12 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_crypto_conf" {
+  bucket = aws_s3_bucket.bucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
