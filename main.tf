52f16af68a09c874f0f3c05b7254adba3f47cbc8
nathandxn <nathan.dixon@snyk.io>
2024-11-10 22:56:40 -0500

    feat: add vpc data blocks

--- main.tf
+++ main.tf
@@ -67,3 +67,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_crypto_con
     }
   }
 }
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnet_ids" "default_subnet" {
+  vpc_id = data.aws_vpc.default_vpc.id
+}
