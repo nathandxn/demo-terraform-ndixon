4ad7f02406652d400d73248984a19f465862ee39
nathandxn <nathan.dixon@snyk.io>
2024-11-29 19:14:02 -0600

    feat: add inbound rule to securit group

--- main.tf
+++ main.tf
@@ -37,6 +37,17 @@ resource "aws_security_group" "alb" {
   name = "alb-security-group"
 }
 
+resource "aws_security_group_rule" "allow_alb_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
 resource "aws_security_group_rule" "allow_alb_all_outbound" {
   type              = "egress"
   security_group_id = aws_security_group.alb.id
